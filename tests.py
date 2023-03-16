2de18d856fc247a5a187322320a4625f04e72257
johnny-sky91 <jantylski91@gmail.com>
2023-03-16 21:24:45 +0100

    some new tests

--- tests.py
+++ tests.py
@@ -4,6 +4,9 @@ from app.models import Component, Component_Comment
 
 component_id = 1
 with app.app_context():
-    component = Component.query.get(component_id)
-    component.component_status = "AAAA"
-    db.session.commit()
+    components = Component.query.order_by(Component.component_id.asc())
+    for x in components:
+        print("Name - ", x.component_name)
+        comments = Component_Comment.query.filter_by(what_component_id=x.component_id)
+        for y in comments:
+            print(y.component_comment_text)
