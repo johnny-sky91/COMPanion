7d1fb690f320d10807f1be3c4a39ec7c420e7a8f
johnny-sky91 <jantylski91@gmail.com>
2023-03-16 20:23:17 +0100

    component list upgrade & template remove

--- app/routes.py
+++ app/routes.py
@@ -20,7 +20,7 @@ def components_list():
     components = Component.query.order_by(Component.component_name.desc())
 
     return render_template(
-        "components_list.html", title="Components", components=components
+        "lists/components_list.html", title="Components", components=components
     )
 
 
@@ -42,7 +42,7 @@ def add_new_component():
         flash(f"A new component has been added - {new_component.component_name}")
         return redirect(url_for("components_list"))
     return render_template(
-        "add_new_component.html", title="Add new Component", form=form
+        "add/add_new_component.html", title="Add new Component", form=form
     )
 
 
@@ -53,7 +53,7 @@ def component_view(component_id):
         what_component_id=component_id
     ).order_by(Component_Comment.component_comment_id.desc())
     return render_template(
-        "component_view.html",
+        "view/component_view.html",
         title=f"{component.component_name}",
         component=component,
         comments_list=comments_list,
@@ -71,7 +71,9 @@ def component_change_status(component_id):
         db.session.commit()
         return redirect(url_for("component_view", component_id=component_id))
     return render_template(
-        "update_component_status.html", title=f"{component.component_name}", form=form
+        "update/update_component_status.html",
+        title=f"{component.component_name}",
+        form=form,
     )
 
 
@@ -89,20 +91,20 @@ def add_component_comment(component_id):
         flash(f"New comment for component has been added")
         return redirect(url_for("component_view", component_id=component_id))
     return render_template(
-        "add_component_comment.html", title=f"{component.component_name}", form=form
+        "add/add_component_comment.html", title=f"{component.component_name}", form=form
     )
 
 
 @app.route("/soi_list")
 def soi_list():
     sois = SOI.query.order_by(SOI.soi_name.desc())
-    return render_template("soi_list.html", title="SOI", sois=sois)
+    return render_template("lists/soi_list.html", title="SOI", sois=sois)
 
 
 @app.route("/soi_view/<soi_id>", methods=["GET", "POST"])
 def soi_view(soi_id):
     soi = SOI.query.get(soi_id)
-    return render_template("soi_view.html", title=f"{soi.soi_name}", soi=soi)
+    return render_template("view/soi_view.html", title=f"{soi.soi_name}", soi=soi)
 
 
 @app.route("/soi_list/add_new_soi", methods=["GET", "POST"])
@@ -114,13 +116,13 @@ def add_new_soi():
         db.session.commit()
         flash(f"A new SOI has been added - {new_soi.soi_name}")
         return redirect(url_for("soi_list"))
-    return render_template("add_new_soi.html", title="Add new SOI", form=form)
+    return render_template("add/add_new_soi.html", title="Add new SOI", form=form)
 
 
 @app.route("/systems_list")
 def systems_list():
     systems = System.query.order_by(System.system_name.desc())
-    return render_template("systems_list.html", title="Systems", systems=systems)
+    return render_template("lists/systems_list.html", title="Systems", systems=systems)
 
 
 @app.route("/systems_list/add_new_system", methods=["GET", "POST"])
@@ -132,7 +134,7 @@ def add_new_system():
         db.session.commit()
         flash(f"A new System has been added - {new_system.system_name}")
         return redirect(url_for("systems_list"))
-    return render_template("add_new_system.html", title="Add new system", form=form)
+    return render_template("add/add_new_system.html", title="Add new system", form=form)
 
 
 # TODO - view - components list
