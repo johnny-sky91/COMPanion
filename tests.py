b987ee37c14d23d8aca8faca381e7f34c3823290
johnny-sky91 <jantylski91@gmail.com>
2023-03-29 12:51:36 +0200

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,5 +1,12 @@
 from app import db, models, app
-from app.models import Component, ComponentComment, SystemComment
+from app.models import (
+    Component,
+    SoiComment,
+    System,
+    SOI,
+    ComponentComment,
+    SystemComment,
+)
 
 
 def comment_query(component_id):
@@ -22,3 +29,71 @@ def test_for_query():
             for x in components_id
         ]
     return comments_list
+
+
+list_comp = ["AAA-111-aaa", "BBB-222-bbb", "CCC-333-ccc", "DDD-444-ddd"]
+list_desc = ["Comp A", "Comp B", "Comp C", "Comp D"]
+list_status = ["Status 1", "Status 2", "Status 2", "Status 3"]
+
+
+def comp_add_test_data(comps, desc, stat):
+    with app.app_context():
+        for c, d, s in zip(comps, desc, stat):
+            new_component = Component(
+                component_name=c,
+                component_description=d,
+                component_status=s,
+            )
+            db.session.add(new_component)
+            db.session.commit()
+
+
+list2_soi = [
+    "SADSD-1",
+    "ASDSAD-2",
+    "DASDDAW-3",
+    "ASDADA-4",
+    "nxcnzc-5",
+    "SAHDHDA-6",
+    "yyyasydayd-7",
+    "TTTASDTT-8",
+]
+list2_desc = ["SOI 1", "SOI 2", "SOI 3", "SOI 4", "SOI 5", "SOI 6", "SOI 7", "SOI 8"]
+list2_status = [
+    "Status 1B",
+    "Status 1B",
+    "Status 1B",
+    "Status 1B",
+    "Status 1B",
+    "Status 1B",
+    "Status 1B",
+    "Status 1B",
+]
+
+
+def soi_add_test_data(sois, desc, stat):
+    with app.app_context():
+        for so, d, s in zip(sois, desc, stat):
+            new_soi = SOI(
+                soi_name=so,
+                soi_description=d,
+                soi_status=s,
+            )
+            db.session.add(new_soi)
+            db.session.commit()
+
+
+def clear_data():
+    with app.app_context():
+        db.session.query(Component).delete()
+        db.session.query(ComponentComment).delete()
+        db.session.query(System).delete()
+        db.session.query(SystemComment).delete()
+        db.session.query(SOI).delete()
+        db.session.query(SoiComment).delete()
+        db.session.commit()
+
+
+clear_data()
+soi_add_test_data(sois=list2_soi, desc=list2_desc, stat=list2_status)
+comp_add_test_data(comps=list_comp, desc=list_desc, stat=list_status)
