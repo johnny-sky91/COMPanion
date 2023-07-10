e8b6637551cd9b41ec5d920af0f453e2412661f5
johnny-sky91 <73224128+johnny-sky91@users.noreply.github.com>
2023-07-10 13:25:53 +0200

    added flask-moment

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import Config
+from flask_moment import Moment
 
 # todo - get __init__ empty
 
 app = Flask(__name__)
+moment = Moment(app)
 
 app.config.from_object(Config)
 db = SQLAlchemy(app)
