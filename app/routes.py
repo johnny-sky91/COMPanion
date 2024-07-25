5e466d272d32ee2280d5e5f9bf0562cdc1ce75e8
Tylski <jantylski91@gmail.com>
2024-07-25 14:28:20 +0200

    main routes imports fix

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
-from app import db
-from flask import current_app as app
+from datetime import datetime
+import json, operator
 
 from flask import (
     render_template,
@@ -7,8 +7,11 @@ from flask import (
     redirect,
     url_for,
     request,
-    make_response,
 )
+import pandas as pd
+
+from app import db
+from flask import current_app as app
 from app.forms import (
     AddComponent,
     AddSOI,
@@ -21,9 +24,9 @@ from app.forms import (
     AddGroup,
     AddGroupProduct,
     AddProductNote,
-    AddGroupsFile,
 )
 from app.models import (
+    tables_dict,
     System,
     SOI,
     Component,
@@ -32,15 +35,10 @@ from app.models import (
     SystemComment,
     ComponentSoi,
     SystemSoi,
-    tables_dict,
     MyGroup,
     MyGroupProduct,
     MyGroupComment,
 )
-from datetime import datetime
-
-import json, io, operator
-import pandas as pd
 
 
 @app.context_processor
@@ -234,20 +232,6 @@ def system_view(id):
         comments=comments,
         sois=sois,
     )
-
-
-# TODO upload excel file
-@app.route("/other", methods=["GET", "POST"])
-def other_view():
-    groups_form = AddGroupsFile()
-    if groups_form.validate_on_submit():
-        file = groups_form.groups_file.data
-        # print(groups_form.groups_file.data)
-        data = pd.read_excel(file)
-        print(data)
-        flash(f"Groups updated!")
-        return redirect(request.referrer)
-    return render_template("other.html", title="Other", groups_form=groups_form)
 
 
 def last_comment(table, products):
@@ -733,239 +717,4 @@ def remove_product_comment(table, table2, product_id, comment_id):
         product_id=product_id, id=comment_id
     ).delete()
     db.session.commit()
-    return redirect(request.referrer)
-
-
-def query_group_name(what_type, product_id):
-    if what_type == "soi":
-        what_group = MyGroupProduct.query.filter_by(soi_id=product_id).first()
-    if what_type == "component":
-        what_group = MyGroupProduct.query.filter_by(component_id=product_id).first()
-    if what_group:
-        group = MyGroup.query.filter_by(id=what_group.my_group_id).first().name
-    else:
-        group = "NA"
-    return group
-
-
-def create_soi_table(sois):
-    soi_groups = [query_group_name("soi", soi.id) for soi in sois]
-    soi_table = pd.DataFrame(
-        {
-            "SOI": [x.name for x in sois],
-            "MATERIAL_NUMBER": [x.material_number for x in sois],
-            "DESCRIPTION": [x.description for x in sois],
-            "STATUS": [x.status for x in sois],
-            "DUMMY": [x.dummy for x in sois],
-            "CHECK": [x.check for x in sois],
-            "GROUP": soi_groups,
-            "NOTE": [x.note for x in sois],
-            "LAST_COMMENT": [
-                x.text if x is not None else ""
-                for x in last_comment(table="soi", products=sois)
-            ],
-        }
-    )
-    return soi_table
-
-
-def create_component_table(components):
-    component_groups = [query_group_name("component", soi.id) for soi in components]
-    component_table = pd.DataFrame(
-        {
-            "COMPONENT": [x.name for x in components],
-            "CODENUMBER": [x.codenumber for x in components],
-            "MATERIAL_NUMBER": [x.material_number for x in components],
-            "DESCRIPTION": [x.description for x in components],
-            "SUPPLIER": [x.supplier for x in components],
-            "STATUS": [x.status for x in components],
-            "CHECK": [x.check for x in components],
-            "GROUP": component_groups,
-            "NOTE": [x.note for x in components],
-            "LAST_COMMENT": [
-                x.text if x is not None else ""
-                for x in last_comment(table="component", products=components)
-            ],
-        }
-    )
-    return component_table
-
-
-def create_group_table(groups):
-    group_table = pd.DataFrame(
-        {
-            "GROUP": [x.name for x in groups],
-            "STATUS": [x.status for x in groups],
-            "CHECK": [x.check for x in groups],
-            "NOTE": [x.note for x in groups],
-            "LAST_COMMENT": [
-                x.text if x is not None else ""
-                for x in last_comment(table="my_group", products=groups)
-            ],
-        }
-    )
-    return group_table
-
-
-def create_system_table(systems):
-    system_table = pd.DataFrame(
-        {
-            "SYSTEM": [x.name for x in systems],
-            "STATUS": [x.status for x in systems],
-            "CHECK": [x.check for x in systems],
-            "NOTE": [x.note for x in systems],
-            "LAST_COMMENT": [
-                x.text if x is not None else ""
-                for x in last_comment(table="system", products=systems)
-            ],
-        }
-    )
-    return system_table
-
-
-def create_bom_table(bom_data):
-    bom = pd.DataFrame(
-        {
-            "SOI": [
-                SOI.query.filter_by(id=row.soi_joint).first().name for row in bom_data
-            ],
-            "COMPONENT": [
-                Component.query.filter_by(id=row.comp_joint).first().name
-                for row in bom_data
-            ],
-            "USAGE": [row.usage for row in bom_data],
-            "MAIN": [row.main for row in bom_data],
-        }
-    )
-    return bom
-
-
-def create_system_soi_table(system_soi_data):
-    system_soi = pd.DataFrame(
-        {
-            "SYSTEM": [
-                System.query.filter_by(id=row.system_joint).first().name
-                for row in system_soi_data
-            ],
-            "SOI": [
-                SOI.query.filter_by(id=row.soi_joint).first().name
-                for row in system_soi_data
-            ],
-        }
-    )
-    return system_soi
-
-
-@app.route("/other/download_data", methods=["GET", "POST"])
-def download_app_data():
-    soi = SOI.query.all()
-    component = Component.query.all()
-    group = MyGroup.query.all()
-    system = System.query.all()
-    system_soi = SystemSoi.query.all()
-    bom = ComponentSoi.query.all()
-
-    soi_table = create_soi_table(soi)
-    component_table = create_component_table(component)
-    group_table = create_group_table(group)
-    system_tabel = create_system_table(system)
-    system_soi_table = create_system_soi_table(system_soi)
-    bom_tabel = create_bom_table(bom)
-
-    now = datetime.now()
-    timestamp = now.strftime("%d%m%y_%H%M")
-    filename = f"COMPanion_data_{timestamp}"
-
-    out = io.BytesIO()
-    writer = pd.ExcelWriter(out, engine="xlsxwriter")
-
-    soi_table.to_excel(excel_writer=writer, index=False, sheet_name="SOI_info")
-    component_table.to_excel(
-        excel_writer=writer, index=False, sheet_name="Component_info"
-    )
-    group_table.to_excel(excel_writer=writer, index=False, sheet_name="Group_info")
-    system_tabel.to_excel(excel_writer=writer, index=False, sheet_name="System_info")
-    system_soi_table.to_excel(excel_writer=writer, index=False, sheet_name="System_SOI")
-    bom_tabel.to_excel(excel_writer=writer, index=False, sheet_name="BOM")
-
-    writer._save()
-
-    download_response = make_response(out.getvalue())
-
-    download_response.headers["Content-Disposition"] = (
-        f"attachment; filename={filename}.xlsx"
-    )
-    download_response.headers["Content-type"] = "application/x-xlsx"
-
-    return download_response
-
-
-@app.route("/other/download_groups_update_file", methods=["GET", "POST"])
-def download_groups_update_file():
-    update_table = pd.DataFrame(columns=["SOI", "COMPONENT", "GROUP"])
-
-    now = datetime.now()
-    timestamp = now.strftime("%d%m%y_%H%M")
-    filename = f"COMPanion_groups_update_{timestamp}"
-
-    out = io.BytesIO()
-
-    writer = pd.ExcelWriter(out, engine="xlsxwriter")
-    update_table.to_excel(excel_writer=writer, index=False, sheet_name="Groups_update")
-    writer._save()
-
-    download_response = make_response(out.getvalue())
-    download_response.headers["Content-Disposition"] = (
-        f"attachment; filename={filename}.xlsx"
-    )
-    download_response.headers["Content-type"] = "application/x-xlsx"
-
-    return download_response
-
-
-def groups_mass_change(group_data):
-    MyGroupProduct.query.delete()
-    db.session.commit()
-    if set(group_data.columns) != set(["SOI", "COMPONENT", "GROUP"]):
-        return f"Wrong file uploaded!"
-    for index, row in group_data.iterrows():
-        soi = SOI.query.filter_by(name=row["SOI"]).first()
-        component = Component.query.filter_by(name=row["COMPONENT"]).first()
-        group = MyGroup.query.filter_by(name=row["GROUP"]).first()
-
-        if not soi:
-            error_message = f"SOI '{row['SOI']}' not found in the database."
-            break
-        if not component:
-            error_message = f"Component '{row['COMPONENT']}' not found in the database."
-            break
-        if not group:
-            error_message = f"Group '{row['GROUP']}' not found in the database."
-            break
-
-        new_product_group = MyGroupProduct(
-            my_group_id=group.id,
-            soi_id=soi.id,
-            component_id=component.id,
-        )
-        db.session.add(new_product_group)
-        db.session.commit()
-    else:
-        return "All groups processed successfully."
-
-    return error_message
-
-
-@app.route("/other/upload_groups_update_file", methods=["GET", "POST"])
-def upload_groups_update_file():
-    form = AddGroupsFile()
-    if form.validate_on_submit():
-        groups_file = form.groups_file.data
-        if groups_file and groups_file.filename.endswith(".xlsx"):
-            data = pd.read_excel(io.BytesIO(groups_file.read()))
-            msg = groups_mass_change(group_data=data)
-            flash(msg)
-            return redirect(request.referrer)
-        else:
-            flash("Please upload a valid Excel file (.xlsx)")
     return redirect(request.referrer)
